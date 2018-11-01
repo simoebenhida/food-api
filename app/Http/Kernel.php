1e09f349cfb25e2c8e7aee1fb34ac888b2806dd1
Abderrhim Soubai <gx.coder@gmail.com>
2018-11-01 20:20:47 +0100

    Fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+
     ];
 
     /**
@@ -38,6 +39,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \Barryvdh\Cors\HandleCors::class,
+
             'throttle:60,1',
             'bindings',
         ],
